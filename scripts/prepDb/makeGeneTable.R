df277ad12152019d8dcbb9704b8da3bc80750c56
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-03-17 00:24:13 +0100

    Now uses a predownload HGNC gene list for converting (potentially) non-standard gene names to ENSEMBL gene ids

--- scripts/prepDb/makeGeneTable.R
+++ scripts/prepDb/makeGeneTable.R
@@ -1,4 +1,4 @@
-library(biomaRt)
+#library(biomaRt)
 library(readxl)
 library(stringr)
 options(stringsAsFactors = F)
@@ -9,11 +9,17 @@ base_dir='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/
 gene_list <- paste0(base_dir,'/scripts_main/hmfGeneAnnotation/data/gene_selection/gene_list.xlsx')
 gene_list <- list(
    cancer = as.data.frame(read_excel(gene_list, sheet = 'cancer_genes')),
-   hr = as.data.frame(read_excel(gene_list, sheet = 'hr_genes'))
+   selected = as.data.frame(read_excel(gene_list, sheet = 'selected_genes'))
 )
 
-hgnc_gene_names <- paste0(base_dir,'/scripts_main/hmfGeneAnnotation/data/gene_selection/hgnc_gene_names_20190205.txt')
+## Split selected genes into HR and non-HR genes
+gene_list$hr <- subset(gene_list$selected, is_hr_gene==T)
+gene_list$other <- subset(gene_list$selected, is_hr_gene==F)
+gene_list$selected <- NULL
+
+hgnc_gene_names <- paste0(base_dir,'/scripts_main/hmfGeneAnnotation/data/gene_selection/hgnc_gene_names_20190311.txt')
 hgnc_gene_names <- read.delim(hgnc_gene_names,comment.char='#')
+colnames(hgnc_gene_names) <- gsub(' ','_',tolower(colnames(GENES_HGNC)))
 
 mart <- useMart(biomart='ensembl', dataset='hsapiens_gene_ensembl', host='grch37.ensembl.org')
 
@@ -27,73 +33,79 @@ mart <- useMart(biomart='ensembl', dataset='hsapiens_gene_ensembl', host='grch37
 
 #========= Main =========#
 getGeneInfo <- function(gene.names){
-   #gene.names=gene_list$cancer$gene
-   #gene.names=gene_list$hr$gene
-   
+
    hgnc_gene_names <- as.data.frame(apply(hgnc_gene_names,2,toupper))
    gene.names <- toupper(gene.names)
-   
+
    ## Convert gene names to hgnc symbol
    findGene <- function(gene.name, col){
       hgnc_gene_names[which(hgnc_gene_names[col] == gene.name),]
    }
-   
+
    hgnc_gene_info <- do.call(rbind, lapply(gene.names, function(i){
       #i=gene.names[38]
       #i='FANCP'
-      
+
       ## Scan all columns
-      df_ss <- findGene(i, 'Approved.symbol')
-      if(nrow(df_ss) == 0){ df_ss <- findGene(i, 'Previous.symbol') }
-      if(nrow(df_ss) == 0){ df_ss <- findGene(i, 'Alias.symbol') }
-      
+      df_ss <- findGene(i, 'approved_symbol')
+      if(nrow(df_ss) == 0){ df_ss <- findGene(i, 'previous_symbols') }
+      if(nrow(df_ss) == 0){ df_ss <- findGene(i, 'synonyms') }
+
       ## Output
       if(nrow(df_ss) == 0){
          out <- data.frame(
             gene.name=i,
             hgnc_id=NA,
             hgnc_symbol=NA
-         ) 
+         )
       } else {
          out <- data.frame(
             gene.name=i,
-            hgnc_id=str_extract(df_ss[1,'HGNC.ID'],'\\d+'),
-            hgnc_symbol=df_ss[1,'Approved.symbol']
-         ) 
+            hgnc_id=str_extract(df_ss[1,'hgnc_id'],'\\d+'),
+            hgnc_symbol=df_ss[1,'approved_symbol']
+         )
       }
-      
+
       return(out)
    }))
 
    ## Query ensembl
    df <- getBM(
-      mart=mart, verbose=F, 
+      mart=mart, verbose=F,
       attributes=c(
          'chromosome_name', 'start_position', 'end_position',
          'hgnc_id','hgnc_symbol','ensembl_gene_id'
-      ), 
-      filters='hgnc_id', 
+      ),
+      filters='hgnc_id',
       values=hgnc_gene_info$hgnc_id
    )
-   
+
    ## Resolve duplicated. Prioritize smaller entrez gene id
    df <- do.call(rbind, lapply(unique(df$ensembl_gene_id), function(i){
       df_ss <- df[df$ensembl_gene_id == i, ]
-      
-      if(nrow(df_ss) == 1){ 
-         return(df_ss) 
-      } else{ 
+
+      if(nrow(df_ss) == 1){
+         return(df_ss)
+      } else{
          return( df_ss[which.min(df_ss$entrezgene),] )
       }
    }))
-   
+
    ## Remove patch entries
    df <- df[grep('PATCH|HSCHR',df$chromosome_name,invert=T),]
-   
+
    ## Reorder
    df <- df[order(df$hgnc_symbol),]
-   
+
    missing_genes <- hgnc_gene_info[!(hgnc_gene_info$hgnc_id %in% df$hgnc_id),'gene.name']
+   
+   if(length(missing_genes) != 0){
+      warning(sprintf(
+         '%s missing genes were found: %s', 
+         length(missing_genes), 
+         paste(missing_genes, collapse=', ')
+      ))
+   }
    
    return(list(
       output = df,
@@ -104,22 +116,27 @@ getGeneInfo <- function(gene.names){
 #========= Final gene table =========#
 ## ignore missing genes
 ## "RAD51L3-RFFL"  "RP11-286N22.8" "38596"         "40057" 
-gene_tables <- list( 
-   hr = getGeneInfo(gene_list$hr$gene)$output,
-   cancer = getGeneInfo(gene_list$cancer$gene)$output
-)
 
-## Remove rows from cancer gene table that are already in hr gene table
-gene_tables$cancer <- gene_tables$cancer[
-   which( !(gene_tables$cancer$hgnc_symbol %in% gene_tables$hr$hgnc_symbol) )
-,]
+gene_table <- do.call(rbind, lapply(names(gene_list), function(i){
+   df <- getGeneInfo(gene_list[[i]]$gene)$output
+   df$is_hr_gene <- ifelse(i=='hr',TRUE,FALSE) ## Mark as hr gene
+   return(df)
+}))
 
-## Mark as hr gene
-gene_tables$hr$is_selected_gene <- TRUE
-gene_tables$cancer$is_selected_gene <- FALSE
+## Remove rows from cancer gene table that are already in gene selection
+gene_table <- gene_table[order(gene_table$is_hr_gene, decreasing=T),]
+gene_table <- gene_table[!duplicated(gene_table$ensembl_gene_id),]
+gene_table <- (function(){
+   df <- gene_table
+   chr_order <- unique(str_sort(df$chromosome_name, numeric=T))
+   do.call(rbind, lapply(chr_order, function(i){
+      df_ss <- df[df$chromosome_name == i,]
+      df_ss[order(df_ss$start_position),]
+   }))
+})()
 
 ## Export bed
-bed <- do.call(rbind, gene_tables)
+bed <- gene_table
 colnames(bed)[1] <- paste0('#',colnames(bed)[1])
 
 write.table(
