03d935599cc5ac3032dffbb26cc8439a609f000e
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-03-17 00:14:00 +0100

    Added bypass mode to ignore the FORMAT fields. The annotated germline vcfs from HMF pipeline 4.8 now only have one FORMAT field (as only germline variants are reported).

--- scripts/preProcHmfOutput/extractVcfFields.sh
+++ scripts/preProcHmfOutput/extractVcfFields.sh
@@ -13,25 +13,46 @@ extractVcfFields(){
 	# txt_out=/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/HMF_update/vcf_subset/CPCT02020719R_CPCT02020719T/CPCT02020719R_CPCT02020719T.germ.txt.gz
 	# mode='germline'
 
-	if [[ $mode == "somatic" ]]; then
-		tumor_col=0
-	elif [[ $mode == "germline" ]]; then
-		tumor_col=1
-	else 
-		echo Available modes are: \"somatic\", \"germline\"
+	if [[ $mode == 'ignore' ]]; then
+		$JAVA -jar $SNPSIFT extractFields $vcf_in \
+			CHROM POS REF ALT \
+			ANN[0].EFFECT \
+			ANN[0].GENE \
+			ANN[0].GENEID \
+			ANN[0].FEATUREID \
+			ANN[0].HGVS_C | 
+			gzip -c > ${txt_out}.temp
+
+
+		zcat ${txt_out}.temp | 
+		awk '{print $0"\t""NA""\t""NA""\t""NA"}' | 
+		sed -e "1s/.*/chrom\tpos\tref\talt\tsnpeff_eff\tsnpeff_gene\tensembl_gene_id\tensembl_transcript_id\thgvs_c\ttumor_gt\ttumor_ad_ref\ttumor_ad_alt/" |
+		gzip -c > $txt_out && rm ${txt_out}.temp
+
+	else
+		if [[ $mode == 'somatic' ]]; then
+			tumor_col=0
+		elif [[ $mode == 'germline' ]]; then
+			tumor_col=1
+		else 
+			echo Available modes are: \"somatic\", \"germline\", \"bypass\"
+		fi
+
+		$JAVA -jar $SNPSIFT extractFields $vcf_in \
+			CHROM POS REF ALT \
+			ANN[0].EFFECT \
+			ANN[0].GENE \
+			ANN[0].GENEID \
+			ANN[0].FEATUREID \
+			ANN[0].HGVS_C \
+			GEN[$tumor_col].GT \
+			GEN[$tumor_col].AD[0] \
+			GEN[$tumor_col].AD[1] |
+		sed -e "1s/.*/chrom\tpos\tref\talt\tsnpeff_eff\tsnpeff_gene\tensembl_gene_id\tensembl_transcript_id\thgvs_c\ttumor_gt\ttumor_ad_ref\ttumor_ad_alt/" |
+		gzip -c > $txt_out
 	fi
 
-	$JAVA -jar $SNPSIFT extractFields $vcf_in \
-		CHROM POS REF ALT \
-		ANN[0].EFFECT \
-		ANN[0].GENE \
-		ANN[0].GENEID \
-		ANN[0].FEATUREID \
-		ANN[0].HGVS_C \
-		GEN[$tumor_col].GT \
-		GEN[$tumor_col].AD[0] GEN[$tumor_col].AD[1] | \
-	sed -e "1s/.*/chrom\tpos\tref\talt\tsnpeff_eff\tsnpeff_gene\tensembl_gene_id\tensembl_transcript_id\thgvs_c\ttumor_gt\ttumor_ad_ref\ttumor_ad_alt/" | \
-	gzip -c > $txt_out
+	
 }
 
 # extractVcfFields \
