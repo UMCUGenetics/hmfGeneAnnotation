e6fb3ef29348381633578ade30fe7bca999a628c
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-03-17 00:35:47 +0100

    Cleaned up deprecated code

--- scripts/annotateGenes/deprecated/mergeGeneStatusTables_exec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rscript=/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfVariantAnnotation/R/mergeGeneStatusTables.R
-
-guixr load-profile ~/.guix-profile/ <<EOF
-Rscript $rscript
-EOF
