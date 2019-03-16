8c3fc2eae5198a99c60e5bffdae2fb2d845cf550
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-03-17 00:20:20 +0100

    Genes bed file now needs to be provided as an argument

--- scripts/preProcHmfOutput/subsetGeneCnv.R
+++ scripts/preProcHmfOutput/subsetGeneCnv.R
@@ -4,18 +4,19 @@ args <- commandArgs(trailingOnly=TRUE)
 
 gene_cnv_path <- args[1]
 out_path <- args[2]
+genes_bed_path <- args[3]
 
 ROOT_DIR <- '/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfGeneAnnotation/'
 GENES_ENST2ENSG <- paste0(ROOT_DIR,'/data/gene_selection/human_genes_enst2ensg.txt.gz')
-GENES_BED <- paste0(ROOT_DIR,'/data/gene_selection/genes.bed')
+#genes_bed_path <- paste0(ROOT_DIR,'/data/gene_selection/genes.bed')
 
 # gene_cnv_path='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/HMF_data/DR010-update/data/170416_HMFregCPCT_FR12245150_FR14064764_CPCT02020493/CPCT02020493T.purple.gene.cnv'
 # GENES_ENST2ENSG='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfGeneAnnotation/data/gene_selection/human_genes_enst2ensg.txt.gz'
-# GENES_BED='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfGeneAnnotation/data/gene_selection/genes.bed'
+# genes_bed_path='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfGeneAnnotation/data/gene_selection/genes.bed'
 
 gene_cnv <- read.delim(gene_cnv_path)
 human_genes_enst2ensg <- read.delim(gzfile(GENES_ENST2ENSG), check.names=F)
-genes_bed <- read.delim(GENES_BED, check.names=F)
+genes_bed <- read.delim(genes_bed_path, check.names=F)
 
 ## Convert colnames from camelcase to snakecase
 colnames(gene_cnv) <- gsub("([a-z])([A-Z])", "\\1_\\L\\2", colnames(gene_cnv), perl = TRUE)
@@ -46,7 +47,7 @@ gene_cnv <- gene_cnv[!(duplicated(gene_cnv$ensembl_gene_id)),]
 
 ## Select relevant rows
 sel_cols <- c(
-   'chromosome','start','end','hgnc_symbol','ensembl_gene_id',
+   'chromosome','start','end','hgnc_symbol','gene','ensembl_gene_id',
    'min_copy_number','max_copy_number','min_minor_allele_ploidy'
 )
 gene_cnv <- gene_cnv[,sel_cols]
