273b721f77b51190607b7400d6e70a1e4c2e7bd6
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-04-15 10:24:32 +0200

    Added function to retrieve gnomad info per variant

--- loadPaths.sh
+++ loadPaths.sh
@@ -13,6 +13,7 @@ ENIGMA_DB=$ROOT_DIR/data/variant_significance/enigma/enigma_variants_20181221.tx
 CADD_DB_SNV=/hpc/cog_bioinf/common_dbs/CADD/whole_genome_SNVs_inclAnno.tsv.gz
 SCAP_DB=$ROOT_DIR/data/variant_significance/SCAP/scap_v1_0.sorted.txt.gz
 MCAP_DB=$ROOT_DIR/data/variant_significance/MCAP/mcap_v1_3.txt.gz
+GNOMAD_DB_DIR=/hpc/cog_bioinf/common_dbs/GNOMAD/v2.1/
 
 #========= Scripts =========#
 ## subset databases
@@ -33,6 +34,7 @@ addSigAnn_sh=$getAnnotations_dir/addSigAnn.sh
 getClinSig_sh=$getAnnotations_dir/getClinSig.sh
 getCaddAnn_py=$getAnnotations_dir/getCaddAnn.py
 getCapAnn_py=$getAnnotations_dir/getCapAnn.py
+getGnomadAnn_py=$getAnnotations_dir/getGnomadAnn.py
 
 ## annotateGenes
 annotateGenesExec_sh=$ROOT_DIR/scripts/annotateGenes/annotateGenesExec.sh
