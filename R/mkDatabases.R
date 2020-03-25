a3ab046924242a89bc6e001f2ccbc647310d802e
Luan Nguyen <nguyen.n.luan777@gmail.com>
2020-03-25 11:56:49 +0100

    - Added code to annotate gene type to gene selection bed file - Added function to make centromere positions table

--- R/mkDatabases.R
+++ R/mkDatabases.R
@@ -153,6 +153,7 @@ mkGenesBed <- function(
 
    cosmic_genes <- read.delim(cosmic.genes.path, stringsAsFactors=F)
    colnames(cosmic_genes) <- gsub('[.]','_',colnames(cosmic_genes))
+   colnames(cosmic_genes) <- gsub('_$','',colnames(cosmic_genes))
    colnames(cosmic_genes) <- tolower(colnames(cosmic_genes))
    
    ensg_ids <- regmatches(cosmic_genes$synonyms, gregexpr('ENSG\\d+[.]', cosmic_genes$synonyms))
@@ -189,11 +190,91 @@ mkGenesBed <- function(
    # anyNA(ensgToHgncSymbol(genes_bed$ensembl_gene_id))
    # anyNA(geneNamesToEnsg(genes_bed$hgnc_symbol))
    
-   genes_bed <- genes_bed[
-      naturalorder(paste0(genes_bed$chromosome_name,'_',genes_bed$start_position))
-   ,]
+   ## Assign TSG or oncogene
+   genes_bed$role_in_cancer <- cosmic_genes$role_in_cancer[ match(genes_bed$ensembl_gene_id, cosmic_genes$ensembl_gene_id) ]
+   genes_bed$role_in_cancer <- gsub(' ','',genes_bed$role_in_cancer)
+   genes_bed$role_in_cancer[nchar(genes_bed$role_in_cancer)==0] <- 'unknown'
+   
+   genes_bed$molecular_genetics <- cosmic_genes$molecular_genetics[ match(genes_bed$ensembl_gene_id, cosmic_genes$ensembl_gene_id) ]
+   genes_bed$molecular_genetics[nchar(genes_bed$molecular_genetics)==0] <- 'unknown'
+   
+   genes_bed <- genes_bed[order(genes_bed$hgnc_symbol),]
    
    colnames(genes_bed)[1:3] <- c('#chrom','start','end')
    
    write.table(genes_bed, out.path, sep='\t', row.names=F, quote=F)
 }
+
+####################################################################################################
+getCentromerePositions <- function(
+   out.path='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfGeneAnnotation/inst/db/centromere_positions_hg19.txt'
+){
+   ## Downloaded from http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/gap.txt.gz
+   gap <- read.table(
+      '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfGeneAnnotation/inst/db/gap.txt.gz',
+      stringsAsFactors=F
+   )
+   gap <- gap[,c(2:4,8)]
+   colnames(gap) <- c('chrom','start','end','gap_type')
+   
+   out <- subset(gap, gap_type=='centromere')
+   out$pos <- (out$start + out$end)/2
+   out <- out[naturalsort::naturalorder(out$chrom),c('chrom','pos')]
+   out$chrom <- gsub('chr','',out$chrom)
+   
+   write.table(out, out.path, sep='\t', row.names=F, quote=F)
+}
+
+# getChromArmCoords <- function(
+#    out.path='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfGeneAnnotation/inst/db/chrom_arms_hg19.txt'
+# ){
+#    ## Not how HMF calculates chrom arms
+#    ## Downloaded from http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/cytoBand.txt.gz
+#    cytobands <- read.table(
+#       '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/hmfGeneAnnotation/inst/db/cytoBand.txt.gz',
+#       stringsAsFactors=F
+#    )
+#    colnames(cytobands) <- c('chrom','start','end','cytoband','stain_value')
+#    cytobands$arm <- substring(cytobands$cytoband,1,1)
+#    cytobands_by_chrom <- split(cytobands, cytobands$chrom)
+# 
+#    arm_coords_by_chrom <- lapply(cytobands_by_chrom, function(i){
+#       #i=cytobands_by_chrom$chr21
+#       df <- do.call(rbind,lapply(split(i, i$arm), function(i){
+#          ## Select 1st and last rows
+#          data.frame(
+#             chrom=i$chrom[1],
+#             arm=i$arm[1],
+#             start=i$start[1]+1, ## Make coords 1 based
+#             end=i$end[nrow(i)]
+#          )
+#       }))
+# 
+#       if(df$chrom[1] %in% paste0('chr',ONE_ARMED_CHROMS)){
+#          df <- data.frame(
+#             chrom=df$chrom[1],
+#             arm='q',
+#             start=i$start[1],
+#             end=i$end[nrow(i)]
+#          )
+#       }
+# 
+#       return(df)
+#    })
+# 
+#    arm_coords_by_chrom <- arm_coords_by_chrom[naturalsort::naturalorder(names(arm_coords_by_chrom))]
+#    out <- do.call(rbind, arm_coords_by_chrom)
+#    rownames(out) <- NULL
+# 
+#    write.table(out, out.path, sep='\t', row.names=F, quote=F)
+# }
+
+
+
+
+
+
+
+
+
+
