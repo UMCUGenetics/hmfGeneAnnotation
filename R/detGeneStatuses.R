e1b05cd68d9efefb80c35abc5695daede798fa16
Luan Nguyen <nguyen.n.luan777@gmail.com>
2020-03-25 12:03:53 +0100

    - Renamed hmf.pl.output.paths to input.file.paths - Added code to execute SNV/indel variant type annotation with snpEff - Added code to summarize biallelic status as a single string

--- R/detGeneStatuses.R
+++ R/detGeneStatuses.R
@@ -1,39 +1,44 @@
 #' Determine gene amplifications and biallelic losses
 #'
 #' @param out.dir Path to output dir
-#' @param hmf.pl.output.paths A list or vector supplying the path output files from the HMF pipeline.
+#' @param input.file.paths A list or vector supplying the path output files from the HMF pipeline.
 #' @param sample.name Name of the sample
 #' This list should be in the form c(germ_vcf='', som_vcf='', gene_cnv='', cnv='')
 #' @param bed.file Path to the bed file containing the genes of interest. This bed file should also 
 #' contain the column ensembl_gene_id
 #' @param java.path Path the the java binary
 #' @param snpsift.path Path to the SnpSift jar
+#' @param do.snpeff.ann Annotate SNV/indels variant type with snpeff?
+#' @param chrom.arm.split.method Can be 'hmf' or 'gap'. Refer to documentation for 
+#' calcChromArmPloidies().
 #' @param verbose Show progress messages?
 #'
 #' @return Writes a diplotypes table to the output dir
 #' @export
 #'
 detGeneStatuses <- function(
-   out.dir, hmf.pl.output.paths=c(germ_vcf='', som_vcf='', gene_cnv='', cnv=''), sample.name,
+   out.dir, input.file.paths=c(germ_vcf='', som_vcf='', gene_cnv='', cnv=''), sample.name,
    bed.file=BED_FILE, java.path=JAVA_PATH, snpsift.path=SNPSIFT_PATH,
+   chrom.arm.split.method='hmf',
+   do.snpeff.ann=F,
    verbose=T
 ){
    
 #========= Debugging =========#
    # if(dir.exists('/Users/lnguyen/')){
-   #    
+   # 
    #    vcf_paths <- read.delim('/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CUPs_classifier/metadata/vcf_paths.txt', stringsAsFactors=F)
-   #    #sample.name <- 'CPCT02380011T' ## BRCA2 LOH+som
+   #    sample.name <- 'CPCT02010422T' ## BRCA2 LOH+som
    #    #sample.name <- 'CPCT02010543T' ## APC som stop gain (5) + som FS (5)
-   #    
+   # 
    #    out.parent.dir <- '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CUPs_classifier_data/gene_ann/'
    #    out.dir <- paste0(out.parent.dir,'/',sample.name,'/')
    #    dir.create(out.dir, recursive=T)
-   #    
-   #    hmf.pl.output.paths <- unlist(vcf_paths[vcf_paths$sample==sample.name,-1])
-   #    hmf.pl.output.paths <- paste0('/Users/lnguyen/',hmf.pl.output.paths)
-   #    names(hmf.pl.output.paths) <- c('germ_vcf','som_vcf','sv_vcf','gene_cnv','cnv')
-   #    
+   # 
+   #    input.file.paths <- unlist(vcf_paths[vcf_paths$sample==sample.name,-1])
+   #    input.file.paths <- paste0('/Users/lnguyen/',input.file.paths)
+   #    names(input.file.paths) <- c('germ_vcf','som_vcf','sv_vcf','gene_cnv','cnv')
+   # 
    #    bed.file=BED_FILE
    #    java.path=JAVA_PATH
    #    snpsift.path=SNPSIFT_PATH
@@ -45,57 +50,58 @@ detGeneStatuses <- function(
       stop('out.dir does not exist: ',out.dir)
    }
    
-   if(length(names(hmf.pl.output.paths))==0){
-      stop('hmf.pl.output.paths must be a named vector or list')
+   if(length(names(input.file.paths))==0){
+      stop('input.file.paths must be a named vector or list')
    }
    
-   if(is.list(hmf.pl.output.paths)){ ## Coerce list to vector
-      hmf.pl.output.paths <- structure(hmf.pl.output.paths, names=names(hmf.pl.output.paths))
+   if(is.list(input.file.paths)){ ## Coerce list to vector
+      input.file.paths <- structure(input.file.paths, names=names(input.file.paths))
    }
    
-   inputs_not_exist <- !file.exists(hmf.pl.output.paths)
+   inputs_not_exist <- !file.exists(input.file.paths)
    if(any(inputs_not_exist)){
       stop(
          'The following files do not exist:\n',
-         paste(hmf.pl.output.paths[!inputs_not_exist],collapse='\n')
+         paste(input.file.paths[!inputs_not_exist],collapse='\n')
       )
    }
    rm(inputs_not_exist)
    
 #========= Pre-process HMF pipeline output =========#
-   hmf_pl_files_proc_dir <- paste0(out.dir,'/hmf_pl_files_proc/')
-   suppressWarnings({ dir.create(hmf_pl_files_proc_dir, recursive=T) })
+   preproc_dir <- paste0(out.dir,'/preproc/')
+   suppressWarnings({ dir.create(preproc_dir, recursive=T) })
    
-   hmf_pl_files_proc_files <- c()
+   preproc_files <- c()
    
    #--------- Gene cnv ---------#
    if(verbose){ message('\n## Subsetting gene cnv...') }
    
-   hmf_pl_files_proc_files['gene_cnv'] <- paste0(hmf_pl_files_proc_dir,'/',sample.name,'.gene.cnv.txt')
+   preproc_files['gene_cnv'] <- paste0(preproc_dir,'/',sample.name,'.gene.cnv.txt')
    
-   if( !file.exists(hmf_pl_files_proc_files['gene_cnv']) ){
+   if( !file.exists(preproc_files['gene_cnv']) ){
       preProcessGeneCnv(
-         gene.cnv.file = hmf.pl.output.paths['gene_cnv'],
-         out.file = hmf_pl_files_proc_files['gene_cnv'],
+         gene.cnv.file = input.file.paths['gene_cnv'],
+         out.file = preproc_files['gene_cnv'],
          bed.file = bed.file,
          verbose = verbose
       )
    } else {
-      if(verbose){ message('Skipping; output file exists: ',hmf_pl_files_proc_files['gene_cnv']) }
+      if(verbose){ message('Skipping; output file exists: ',preproc_files['gene_cnv']) }
    }
    
    #--------- Cnv ---------#
    if(verbose){ message('\n## Calculating chrom arm ploidies...') }
-   hmf_pl_files_proc_files['arm_ploidies'] <- paste0(hmf_pl_files_proc_dir,'/',sample.name,'.chrom_arm_ploidies.txt')
+   preproc_files['arm_ploidies'] <- paste0(preproc_dir,'/',sample.name,'.chrom_arm_ploidies.txt')
    
-   if( !file.exists(hmf_pl_files_proc_files['arm_ploidies']) ){
+   if( !file.exists(preproc_files['arm_ploidies']) ){
       calcChromArmPloidies(
-         hmf.pl.output.paths['cnv'],
-         out.file = hmf_pl_files_proc_files['arm_ploidies'],
+         input.file.paths['cnv'],
+         out.file = preproc_files['arm_ploidies'],
+         chrom.arm.split.method=chrom.arm.split.method,
          verbose = verbose
       )
    } else {
-      if(verbose){ message('Skipping; output file exists: ',hmf_pl_files_proc_files['arm_ploidies']) }
+      if(verbose){ message('Skipping; output file exists: ',preproc_files['arm_ploidies']) }
    }
    
    #--------- Somatic/germline vcfs ---------#
@@ -110,37 +116,47 @@ detGeneStatuses <- function(
       VARNAME_vcf <- paste0(mut_type,'_vcf')
       VARNAME_txt <- paste0(mut_type,'_txt')
       
-      hmf_pl_files_proc_files[VARNAME_vcf] <- paste0(hmf_pl_files_proc_dir,'/',sample.name,'.',mut_type,'.vcf.gz')
-      hmf_pl_files_proc_files[VARNAME_txt] <- paste0(hmf_pl_files_proc_dir,'/',sample.name,'.',mut_type,'.txt.gz')
+      preproc_files[VARNAME_vcf] <- paste0(preproc_dir,'/',sample.name,'.',mut_type,'.vcf.gz')
+      preproc_files[VARNAME_txt] <- paste0(preproc_dir,'/',sample.name,'.',mut_type,'.txt.gz')
       
       if(verbose){ message('\n## Processing ',toupper(mut_type),' vcf...') }
       
-      ## Subsetting
-      if( !file.exists(hmf_pl_files_proc_files[VARNAME_vcf]) ){
+      if( !file.exists(preproc_files[VARNAME_vcf]) ){
          if(verbose){ message('> Subsetting on genes in bed file...') }
          filterVcf(
-            vcf.file = hmf.pl.output.paths[VARNAME_vcf],
-            out.file = hmf_pl_files_proc_files[VARNAME_vcf],
+            vcf.file = input.file.paths[VARNAME_vcf],
+            out.file = preproc_files[VARNAME_vcf],
             mode = mut_type,
             bed.file = bed.file,
             java.path = java.path,
             snpsift.path = snpsift.path
          )
       } else {
-         if(verbose){ message('> Skipping subsetting vcf; output file exists: ',hmf_pl_files_proc_files[VARNAME_vcf]) }
+         if(verbose){ message('> Skipping subsetting vcf; output file exists: ',preproc_files[VARNAME_vcf]) }
+      }
+      
+      ## Snpeff annotation
+      if(do.snpeff.ann){
+         annotateVariantType(
+            vcf.file=preproc_files[VARNAME_vcf], 
+            out.file=preproc_files[VARNAME_vcf],
+            genome='GRCh37.75',
+            java.path=JAVA_PATH, 
+            snpeff.path=SNPEFF_PATH
+         )
       }
       
       ## Filtering
-      if( !file.exists(hmf_pl_files_proc_files[VARNAME_txt]) ){
+      if( !file.exists(preproc_files[VARNAME_txt]) ){
          if(verbose){ message('> Extracting revelant fields in vcf to txt...') }
          extractVcfFields(
-            vcf.file = hmf_pl_files_proc_files[VARNAME_vcf],
-            out.file = hmf_pl_files_proc_files[VARNAME_txt],
+            vcf.file = preproc_files[VARNAME_vcf],
+            out.file = preproc_files[VARNAME_txt],
             java.path = java.path,
             snpsift.path = snpsift.path
          )
          
-         txt <- read.delim(hmf_pl_files_proc_files[VARNAME_txt], stringsAsFactors=F)
+         txt <- read.delim(preproc_files[VARNAME_txt], stringsAsFactors=F)
          
          if(verbose){ message('> Performing extra subsetting for ENSG ids present in bed file...') }
          txt <- txt[txt$ensembl_gene_id %in% bed_file$ensembl_gene_id,]
@@ -151,11 +167,11 @@ detGeneStatuses <- function(
          if(verbose){ message('> Adding ClinVar annotations...') }
          txt$clinvar_sig <- getClinSig(txt, CLINVAR_PATH)
          
-         write.tsv(txt, hmf_pl_files_proc_files[VARNAME_txt])
+         write.tsv(txt, preproc_files[VARNAME_txt])
          rm(txt)
          
       } else {
-         if(verbose){ message('> Skipping making txt; output file exists: ',hmf_pl_files_proc_files[VARNAME_txt]) }
+         if(verbose){ message('> Skipping making txt; output file exists: ',preproc_files[VARNAME_txt]) }
       }
    }
    
@@ -165,17 +181,17 @@ detGeneStatuses <- function(
    input_table_names <- c('arm_ploidies','gene_cnv','som_txt','germ_txt')
    
    input_tables <- lapply(input_table_names, function(i){ 
-         message('Reading file: ', hmf_pl_files_proc_files[i] )
-         read.delim(hmf_pl_files_proc_files[i], stringsAsFactors=F) 
+         message('Reading file: ', preproc_files[i] )
+         read.delim(preproc_files[i], stringsAsFactors=F) 
       }
    )
    names(input_tables) <- input_table_names
    rm(input_table_names)
    
-   # input_tables$arm_ploidies <- read.delim(hmf_pl_files_proc_files['arm_ploidies'], stringsAsFactors=F)
-   # input_tables$gene_cnv     <- read.delim(hmf_pl_files_proc_files['gene_cnv'], stringsAsFactors=F)
-   # input_tables$germ         <- read.delim(hmf_pl_files_proc_files['germ_txt'], stringsAsFactors=F)
-   # input_tables$som          <- read.delim(hmf_pl_files_proc_files['som_txt'], stringsAsFactors=F)
+   # input_tables$arm_ploidies <- read.delim(preproc_files['arm_ploidies'], stringsAsFactors=F)
+   # input_tables$gene_cnv     <- read.delim(preproc_files['gene_cnv'], stringsAsFactors=F)
+   # input_tables$germ         <- read.delim(preproc_files['germ_txt'], stringsAsFactors=F)
+   # input_tables$som          <- read.delim(preproc_files['som_txt'], stringsAsFactors=F)
    
    #--------- Make mut profiles ---------#
    mut_profile_dir <- paste0(out.dir,'/mut_profiles/')
@@ -187,7 +203,7 @@ detGeneStatuses <- function(
    )
    
    if( all(file.exists(unlist(mut_profile_paths))) ){
-      mut_profile <- lapply(mut_profile_paths, read.delim)
+      mut_profile <- lapply(mut_profile_paths, read.delim, stringsAsFactors=F)
    } 
    
    else {
@@ -272,6 +288,7 @@ detGeneStatuses <- function(
    #========= Calculate hit scores and determine gene max effect =========#
    if(verbose){ message('\n## Merging diplotype origins into one table...') }
    gene_diplotypes <- do.call(rbind, l_gene_diplotypes)
+   rownames(gene_diplotypes) <- NULL
    
    ## Fix to remove duplicate rows for deep_deletion and trunc (due to being reported in both cnv_germ and cnv_som)
    gene_diplotypes <- unique(gene_diplotypes)
@@ -284,12 +301,13 @@ detGeneStatuses <- function(
    )
    
    if(verbose){ message('\n## Determining biallelic hit type...') }
-   gene_diplotypes <- insColAfter(
-      gene_diplotypes,
-      detHitType(gene_diplotypes),
-      after='diplotype_origin',
-      colname='hit_type'
-   )
+   gene_diplotypes <- (function(){
+      df <- data.frame(
+         hit_type=detBiallHitType(gene_diplotypes),
+         biallelic_status=detBiallStatus(gene_diplotypes)
+      )
+      insColAfter(gene_diplotypes, df, after='diplotype_origin')
+   })()
    
    if(verbose){ message('\n## Determining most pathogenic diplotype per gene...') }
    gene_diplotypes_max <- (function(){
@@ -309,11 +327,11 @@ detGeneStatuses <- function(
    gene_diplotypes_max <- (function(){
       df <- mut_profile$gene_cnv[
          match(gene_diplotypes_max$ensembl_gene_id, mut_profile$gene_cnv$ensembl_gene_id),
-         c('amp_ratio_local','amp_ratio_arm','amp_ratio','amp_context')
+         c('amp_ratio_arm','amp_ratio_local','amp_ratio')
       ]
       insColAfter(gene_diplotypes_max, df, after='hgnc_symbol')
    })()
-
+   
    #========= Export diplotype tables =========#
    if(verbose){ message('\n## Exporting gene diplotype tables...') }
    write.tsv(gene_diplotypes,paste0(out.dir,'/gene_diplotypes_full.txt.gz'))
